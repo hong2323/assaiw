8bbf35661b051f29844b910ca5d4b443a975dd4b
hong2323 <42904811+hong2323@users.noreply.github.com>
2019-10-12 22:59:43 +0700

    Delete index.js

--- js/index.js
+++
@@ -1,9 +0,0 @@
-function navigation(){
-	$('.primary-nav').css('height', $('.logo').height());
-	$('.primary-nav li').css('margin-top', ($('.primary-nav').height()-$('.primary-nav li').height())/2 + 'px')
-
-	$(window).resize(function(){
-		setTimeout(navigation, 500);
-	});
-}
-
